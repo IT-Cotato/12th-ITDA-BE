2c8ad343b65fa1c7e56f225e625d5059e8112f83
HaewonLee <ttoodong@naver.com>
2025-11-23 19:11:56 +0900

    chore: Java 버전 변경(17) 및 이슈 템플릿 수정

--- src/main/java/com/cotato/itda/ItdaApplication.java
+++ src/main/java/com/cotato/itda/ItdaApplication.java
@@ -2,10 +2,9 @@ package com.cotato.itda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 // DB 세팅되면 exclude 옵션 제거하기
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(excludeName = {"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"})
 public class ItdaApplication {
 
 	public static void main(String[] args) {
