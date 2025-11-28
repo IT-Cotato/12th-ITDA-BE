0fe1c215fe5eae1ae04ae9ead120621423184dee
ccjngwn <ahaha620@gmail.com>
2025-11-28 09:34:16 +0900

    chore: CI/CD 파이프라인 구축 및 배포 환경 설정

--- src/main/java/com/cotato/itda/ItdaApplication.java
+++ src/main/java/com/cotato/itda/ItdaApplication.java
@@ -3,8 +3,7 @@ package com.cotato.itda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// DB 세팅되면 exclude 옵션 제거하기
-@SpringBootApplication(excludeName = {"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"})
+@SpringBootApplication
 public class ItdaApplication {
 
 	public static void main(String[] args) {
