c6cdb4659d11fffa493ab5fe5b7a37e67533a3c0
GiminKim <gimin1463@naver.com>
2025-11-28 11:11:08 +0900

    fix: ErrorCode 수정

--- src/main/java/com/cotato/itda/global/error/constant/ErrorCode.java
+++ src/main/java/com/cotato/itda/global/error/constant/ErrorCode.java
@@ -1,9 +1,11 @@
 package com.cotato.itda.global.error.constant;
 
+import org.springframework.http.HttpStatus;
+
 public interface ErrorCode {
 	String getCode();
 
 	String getMessage();
 
-	String getHttpStatus();
+	HttpStatus getHttpStatus();
 }
