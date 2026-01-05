16d853fb3fcaa2e5c22764d4a70fa0e3b724b381
Gimin Kim <gimin1463@naver.com>
2026-01-05 22:52:47 +0900

    fix: 파일 삭제

--- src/main/java/com/cotato/itda/domain/signup/dto/Data.java
+++
@@ -1,5 +0,0 @@
-package com.cotato.itda.domain.signup.dto;
-
-import java.util.List;
-
-public record Data(List<TermsItem> terms){}
--- src/main/java/com/cotato/itda/domain/signup/dto/Flags.java
+++
@@ -1,5 +0,0 @@
-package com.cotato.itda.domain.signup.dto;
-
-import com.cotato.itda.domain.signup.model.TermsBundle;
-
-public record Flags(TermsBundle termsBundle){}
