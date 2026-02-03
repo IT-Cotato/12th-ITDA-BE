77a95fe3164a111447b297d0822458a954dbd5e1
Gimin Kim <gimin1463@naver.com>
2026-02-03 10:34:21 +0900

    fix: ChatRoom Entity @Getter 추가

--- src/main/java/com/cotato/itda/domain/chat/entity/ChatRoom.java
+++ src/main/java/com/cotato/itda/domain/chat/entity/ChatRoom.java
@@ -17,8 +17,10 @@ import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(
