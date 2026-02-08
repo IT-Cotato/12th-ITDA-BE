ebfd902a8b7635f149830a5c3e7c172ffda46093
Gimin Kim <gimin1463@naver.com>
2026-02-08 17:57:52 +0900

    feat: code로 ChatTopic 조회 추가

--- src/main/java/com/cotato/itda/domain/chattopic/repository/ChatTopicRepository.java
+++ src/main/java/com/cotato/itda/domain/chattopic/repository/ChatTopicRepository.java
@@ -4,9 +4,12 @@ import com.cotato.itda.domain.chattopic.entity.ChatTopic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatTopicRepository extends JpaRepository<ChatTopic, Long> {
     List<ChatTopic> findAllByCodeIn(List<String> codes);
 
     List<ChatTopic> findAllByIsActiveTrue();
+
+    Optional<ChatTopic> findByCodeAndIsActiveTrue(String code);
 }
