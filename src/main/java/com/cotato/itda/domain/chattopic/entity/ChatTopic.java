fbd2abae235971d23b3136de7e1ed3a22286065a
Gimin Kim <gimin1463@naver.com>
2026-02-08 17:56:35 +0900

    fix: ChatTopic code Unique 추가

--- src/main/java/com/cotato/itda/domain/chattopic/entity/ChatTopic.java
+++ src/main/java/com/cotato/itda/domain/chattopic/entity/ChatTopic.java
@@ -4,6 +4,7 @@ import com.cotato.itda.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.*;
 
 @Entity
@@ -11,7 +12,11 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-@Table(name = "chat_topic")
+@Table(name = "chat_topic",
+    uniqueConstraints= {
+        @UniqueConstraint(name = "uk_chat_topic_code", columnNames = {"code"})
+    }
+)
 public class ChatTopic extends BaseEntity {
 
     @Column(name = "name", nullable = false)
