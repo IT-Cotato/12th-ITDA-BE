57e061463c9bccfbbed50933c4f824da6e8b7a45
Gimin Kim <gimin1463@naver.com>
2026-02-11 15:36:37 +0900

    feat: memberRepository.java 메서드 추가 - findByPhoneNumberAndName(String, String) - updatePasswordByPhoneNumber(String, String)

--- src/main/java/com/cotato/itda/domain/member/repository/MemberRepository.java
+++ src/main/java/com/cotato/itda/domain/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.cotato.itda.domain.member.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cotato.itda.domain.member.entity.Member;
 
@@ -10,4 +12,16 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findByPhoneNumber(String phoneNumber);
 
 	Boolean existsByPhoneNumber(String phoneNumber);
+
+	// 핸드폰 번호와 이름으로 조회
+	Optional<Member> findByPhoneNumberAndName(String phoneNumber, String name);
+
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
+	@Query("""
+        update Member m
+           set m.passwordHash = :encodedPassword
+         where m.phoneNumber = :phoneNumber
+    """)
+	void updatePasswordByPhoneNumber(String encodedPassword, String phoneNumber);
+
 }
