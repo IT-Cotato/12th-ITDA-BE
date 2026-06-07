093e148a92f06bc4d192880f106e389e80fe70bf
Haewon Lee <ttoodong@naver.com>
2026-06-07 20:41:36 +0900

    feat: 챌린지 대댓글 작성, 삭제 API 구현

--- src/main/java/com/cotato/itda/domain/challenge/entity/ChallengeComment.java
+++ src/main/java/com/cotato/itda/domain/challenge/entity/ChallengeComment.java
@@ -1,12 +1,16 @@
 package com.cotato.itda.domain.challenge.entity;
 
+import com.cotato.itda.domain.diary.entity.DiaryComment;
+import com.cotato.itda.domain.diary.exception.code.DiaryErrorCode;
 import com.cotato.itda.domain.member.entity.Member;
 import com.cotato.itda.global.entity.BaseEntity;
+import com.cotato.itda.global.error.exception.BusinessException;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Builder
@@ -14,7 +18,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Table(name = "challenge_comment")
-@SQLRestriction("is_deleted = false")
 public class ChallengeComment extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -35,17 +38,28 @@ public class ChallengeComment extends BaseEntity {
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
-    // 추후 대댓글 기능 도입 시 사용
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "parent_id")
-//    private ChallengeComment parentComment;
-//
-//    @OneToMany(mappedBy = "parentComment")
-//    @Builder.Default
-//    private List<ChallengeComment> childComments = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private ChallengeComment parentComment;
+
+    @OneToMany(mappedBy = "parentComment")
+    @Builder.Default
+    private List<ChallengeComment> childComments = new ArrayList<>();
 
     public void delete() {
         this.isDeleted = true;
         this.deletedAt = LocalDateTime.now();
+    }
+
+    public void setParent(ChallengeComment parent) {
+        if (parent == null) {
+            return;
+        }
+        // depth 2 제한
+        if (parent.getParentComment() != null) {
+            throw new BusinessException(DiaryErrorCode.REPLY_DEPTH_LIMIT);
+        }
+        this.parentComment = parent;
+        parent.getChildComments().add(this);
     }
 }
